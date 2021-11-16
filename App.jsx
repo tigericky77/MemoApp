a328c32067a94038fb919c6c4b96f1104f86a298
tony <41285773+tigericky77@users.noreply.github.com>
2021-11-17 00:20:34 +1300

    Install @expo/vector-icons

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 // eslint-disable-next-line react/function-component-definition
 export default function App() {
   return (
-    <MemoDetailScreen />
+    <MemoListScreen />
   );
 }
