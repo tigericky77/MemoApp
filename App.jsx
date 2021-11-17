578a66cf9e8e78225acd66b6ff1c9b8b7eea2438
tony <41285773+tigericky77@users.noreply.github.com>
2021-11-17 21:14:06 +1300

    Replace KeyboardAvoidingView with KeyboardSafeView

--- App.jsx
+++ App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 // eslint-disable-next-line react/function-component-definition
 export default function App() {
   return (
-    <MemoCreateScreen />
+    // <MemoCreateScreen />
+    <MemoEditScreen />
   );
 }
--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import {
-  View, TextInput, StyleSheet, KeyboardAvoidingView,
+  View, TextInput, StyleSheet,
 } from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
+import KeyboardSafeView from '../components/KeyboardSafeView';
 
 // eslint-disable-next-line react/function-component-definition
 export default function MemoEditScreen() {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="height">
+    <KeyboardSafeView style={styles.container} behavior="height">
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="買い物リスト" multiline style={styles.input} />
       </View>
       <CircleButton name="check" />
-    </KeyboardAvoidingView>
+    </KeyboardSafeView>
   );
 }
 
